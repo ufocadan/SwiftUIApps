56779baccd4b310b4827de64dbdd53d08ab37cfc
unalfocadan <unalfocadan@gmail.com>
2024-06-10 23:35:01 +0300

    feat: Add LoginView and integrate navigation from WelcomeView
    - Created LoginView with user interface elements (username, password fields, and login button)
    - Added navigation from WelcomeView to LoginView via Get Started button
    - Implemented basic validation for login inputs
    - Updated storyboard to include LoginView
    - Enhanced UI with initial styling for a better user experience
    
    #3daysprogress #iOSDevelopment

---
+++ onlineappwithunal/onlineappwithunal/UICommon/Globals.swift
@@ -0,0 +1,18 @@
+//
+//  Globals.swift
+//  onlineappwithunal
+//
+//  Created by Ünal Foçadan on 10.06.2024.
+//
+
+import SwiftUI
+
+struct Globals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Globals()
+}
